9720545cfaec6dadc42d0acf0f6c87310ba1f325
nixonbii2@gmail.com <nixonbii2@gmail.com>
2018-07-23 23:31:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ function subtract(a, b) {
   return a - b;
 }
 
+function multiply( a, b) {
+  return a * b;
+}
 
+function divide(a, b) {
+  return a / b;
+}
 
 function makeInt(n) {
  return parseInt(n);
